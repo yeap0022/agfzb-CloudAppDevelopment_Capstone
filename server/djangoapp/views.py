fecfff566bcbf2595eacb8458c54ed4a9dc2a1af
yeap0022 <eric_yeap_90@hotmail.com>
2022-11-02 03:58:33 -0400

    updated view.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -34,7 +34,7 @@ def about(request):
 def contact(request):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/contactus.html', context)
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
